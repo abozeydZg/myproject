da02b396abfdf42d4db24cc87153a6c8d6cfd38a
abozeydZg <62264817+abozeydZg@users.noreply.github.com>
2024-01-02 19:52:21 +0100

    Signed-off-by: abozeydZg <62264817+abozeydZg@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb+srv://azzeddaoui:%40A1971*.@cluster0.5mocllq.mongodb.net/?retryWrites=true&w=majority').then(() => {
+    console.log('Connected to MongoDB');  
+});
+
+const  Usermodel = require('./users');
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/',async (req, res) => {
+    const user = await Usermodel.find();
+  res.json(user)
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
